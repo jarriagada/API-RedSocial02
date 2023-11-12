d6abdd2171aa147b297f89ccab6053251a22ed29
jarriagada <jlas1680@gmail.com>
2023-11-11 21:23:46 -0300

    feat: add metodo save follow

--- controllers/follow.js
+++ controllers/follow.js
@@ -14,6 +14,45 @@ const pruebaFollow = (req, res) =>{
 } ;
 
 //Metodo de guardar un follow o seguir
+const save = (req, res) => {
+    //conseguir los datos del body
+    const params = req.body;
+
+    //Sacar el id del usuario identificado
+    const identity = req.user;
+
+    //crear el objeto con modelo follow
+    let userToFollow = new Follow({
+        user: identity.id,
+        followed: params.followed
+    });
+
+    //guardar objeto en BD
+    userToFollow.save((error, followStored ) => {
+
+        if(error || !followStored) {
+            return res.status(500).json({
+                status: "success",
+                message: "No se ha podido seguir al usuario"
+
+            })
+           
+
+        }
+
+        return res.status(200).json({
+            status: "success",
+            identity: req.user,
+            follow: userToFollow
+
+        })
+
+    })
+
+
+    
+
+}
 
 //Metodo de borrar un follow o dejar de seguir
 
@@ -24,5 +63,6 @@ const pruebaFollow = (req, res) =>{
 
 //Exportar acciones
 module.exports ={ 
-    pruebaFollow
+    pruebaFollow,
+    save
 }
--- routes/follow.js
+++ routes/follow.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const auth = require('../middleware/auth');
 
 const followController = require("../controllers/follow");
 
 //Definir las rutas
-router.get("/prueba-follow", followController.pruebaFollow)
+router.get("/prueba-follow", auth, followController.pruebaFollow);
+//post para enviar y guardar follow
+router.post("/save", auth, followController.save);
 
 
 //Exportar router
